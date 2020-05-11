a11511e653269382b57c2cab9afbc5d1aea2f164
Christopher Schuster <schustrchr@gmail.com>
2020-05-11 12:03:28 +0200

    Added table support for console

--- assistance/smart_assistant.py
+++ assistance/smart_assistant.py
@@ -1,7 +1,7 @@
 from data.storage import InteractiveDataStorage
 from moodle.api import MoodleSession
 from muesli.api import MuesliSession
-from util.console import ConsoleFormatter
+from util.console import ConsoleFormatter, string_table
 
 
 class SmartAssistant:
@@ -22,9 +22,9 @@ class SmartAssistant:
         print()
 
     def _print_header(self, title):
-        self._printer.inform('┌' + '─' * 88 + '┐')
-        self._printer.inform(f'│{title:^88}│')
-        self._printer.inform('└' + '─' * 88 + '┘')
+        self._printer.inform('┌' + '─' * 120 + '┐')
+        self._printer.inform(f'│{title:^120}│')
+        self._printer.inform('└' + '─' * 120 + '┘')
 
     def _open_connection_to(self, session):
         self._printer.inform(f'Current state: {session.name}')
@@ -43,11 +43,32 @@ class SmartAssistant:
         self._printer.inform()
 
     def hello(self):
-        self._print_header(f'{"Hello, " + self._storage.my_name_alias:^88}')
-        my_tutorials = self._storage.my_tutorials
-        for my_tutorial in my_tutorials:
+        def my_tutorial_to_str(my_tutorial):
             students = self._storage.get_all_students_of_tutorial(my_tutorial.tutorial_id)
-            self._printer.inform(f'{my_tutorial.time} {my_tutorial.location} [{len(students)}]')
+            return f'({my_tutorial.tutorial_id}) ' \
+                   f'{my_tutorial.time} {my_tutorial.location:<15}' \
+                   f'[{len(students)}]'
+
+        def other_tutorial_to_str(other_tutorial):
+            students = self._storage.get_all_students_of_tutorial(other_tutorial.tutorial_id)
+            tutor = other_tutorial.tutor.split()
+            tutor = f'{tutor[0]} {tutor[-1]}'
+            return f'({other_tutorial.tutorial_id}) ' \
+                   f'{other_tutorial.time} {other_tutorial.location:<15} ' \
+                   f'{tutor:25}' \
+                   f'[{len(students)}]'
+
+        self._print_header(f'{"Hello, " + self._storage.my_name_alias:^88}')
+        headers = ["My Tutorials", "Other Tutorials"]
+        my_tutorials = self._storage.my_tutorials
+        my_tutorials = [my_tutorial_to_str(my_tutorial) for my_tutorial in my_tutorials]
+        other_tutorials = self._storage.other_tutorials
+        other_tutorials = [other_tutorial_to_str(other_tutorial) for other_tutorial in other_tutorials]
+        table = string_table(headers, [my_tutorials, other_tutorials])
+
+        with self._printer as printer:
+            for line in table:
+                printer.inform(line)
         self._printer.inform()
 
         self._printer.inform("Enter a command or use 'help' / '?' to list all available commands.")
--- util/console.py
+++ util/console.py
@@ -5,6 +5,14 @@ def clear():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 
+def colored_string(message, color):
+    colors = {'red':124, 'orange':202, 'green':40}
+    if type(color) == str:
+        color = colors[color]
+
+    return f'\033[38;5;{color}m{message}\033[0m'
+
+
 class ConsoleFormatter:
     def __init__(self):
         self._indentation_level = 0
@@ -25,13 +33,13 @@ class ConsoleFormatter:
         print(self.indentation + message, end=end)
 
     def confirm(self, message, end='\n'):
-        print(self.indentation + '\033[38;5;40m' + message + '\033[0m', end=end)
+        print(self.indentation + colored_string(message, 'green'), end=end)
 
     def warning(self, warning, end='\n'):
-        print(self.indentation + '\033[38;5;202m' + warning + '\033[0m', end=end)
+        print(self.indentation + colored_string(warning, 'orange'), end=end)
 
     def error(self, error, end='\n'):
-        print(self.indentation + '\033[38;5;124m' + error + '\033[0m', end=end)
+        print(self.indentation + colored_string(error, 'red'), end=end)
 
     def __enter__(self):
         self.indent()
@@ -39,3 +47,32 @@ class ConsoleFormatter:
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.outdent()
+
+def string_table(header, columns, align_header='^', align_row='>'):
+    def pad_entry(entry, size, align):
+        return ("{:" + f'{align}{size}' + "}").format(entry)
+
+    def get_or_default(li, index, default=' '):
+        if 0 <= index < len(li):
+            return li[index]
+        else:
+            return default
+
+    if len(header) != len(columns):
+        raise ValueError('Need the same amount of columns than headers!')
+
+    columns = [[str(entry) for entry in column] for column in columns]
+    max_width = [0] * len(header)
+    for i, column in enumerate(columns):
+        if len(column) == 0:
+            max_width[i] = len(header[i])
+        else:
+            max_width[i] = max(len(header[i]), *[len(_) for _ in column])
+
+    table = list()
+    table.append(' │ '.join([pad_entry(entry, max_width[i], align_header) for i, entry in enumerate(header)]))
+    table.append('─┼─'.join(['─'* max_width[i] for i in range(len(header))]))
+    for col_index in range(max(map(len, columns))):
+        table.append(' │ '.join([pad_entry(get_or_default(columns[i], col_index), max_width[i], align_row) for i in range(len(header))]))
+
+    return table
